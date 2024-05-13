4912c95a2dd5a0d4e96836baae85536ade356e9a
chojeongeun <juy0816@naver.com>
2024-05-13 16:50:49 +0900

    내부 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import { Component } from "react";
 import "./App.css";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
 
 export default class App extends Component {
   render() {
     return (
       <main className="main-container">
-        <div className="sub-contaienr">
+        <div className="sub-container">
           <h1>장바구니</h1>
 
           <div
             style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}
           >
-            {/* Expense Form */}Expense Form
+            {/* Expense Form */}
+            <ExpenseForm />
           </div>
 
           <div
             style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}
           >
-            {/* Expense List */}Expense List
+            {/* Expense List */}
+            <ExpenseList />
           </div>
 
           <div
