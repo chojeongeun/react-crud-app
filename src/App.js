4b3f6c1d8e75b638d74603129df6fe2bac1bf667
chojeongeun <juy0816@naver.com>
2024-05-16 18:07:12 +0900

    Create 기능 생성

--- src/App.js
+++ src/App.js
@@ -1,62 +1,84 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 import "./App.css";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      expenses: [
-        { id: 1, charge: "콜라", amount: 2000 },
-        { id: 2, charge: "빵", amount: 1000 },
-        { id: 3, charge: "맥북", amount: 20000 },
-      ],
-    };
-  }
+const App = () => {
+  const [expenses, setExpenses] = useState([
+    { id: 2, charge: "빵", amount: 1000 },
+    { id: 3, charge: "맥북", amount: 20000 },
+  ]);
 
-  handleDelete = (id) => {
-    const newExpense = this.state.expenses.filter(
-      (expense) => expense.id !== id
-    );
-    this.setState({ expenses: newExpense });
-    console.log("newExpense", newExpense);
+  const [charge, setCharge] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const handleCharge = (e) => {
+    setCharge(e.target.value);
   };
 
-  render() {
-    return (
-      <main className="main-container">
-        <div className="sub-container">
-          <h1>장바구니</h1>
+  const handleAmount = (e) => {
+    console.log(e.target.valueAsNumber);
+    setAmount(e.target.valueAsNumber);
+  };
 
-          <div
-            style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}
-          >
-            {/* Expense Form */}
-            <ExpenseForm />
-          </div>
+  const handleDelete = (id) => {
+    const newExpense = expenses.filter((expense) => expense.id !== id);
 
-          <div
-            style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}
-          >
-            {/* Expense List */}
-            <ExpenseList
-              initialExpenses={this.state.expenses}
-              handleDelete={this.handleDelete}
-            />
-          </div>
+    setExpenses(newExpense);
+    this.setState({ expenses: newExpense });
+  };
 
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "start",
-              marginTop: "1rem",
-            }}
-          >
-            <p style={{ fontSize: "2rem" }}>총합계:</p>
-          </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (charge !== "" && amount > 0) {
+      const newExpense = { id: crypto.randomUUID(), charge, amount };
+
+      const newExpenses = [...expenses, newExpense];
+      setExpenses(newExpenses);
+      setCharge("");
+      setAmount(0);
+    } else {
+      console.error("error");
+    }
+  };
+
+  return (
+    <main className="main-container">
+      <div className="sub-container">
+        <h1>장바구니</h1>
+
+        <div
+          style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}
+        >
+          {/* Expense Form */}
+          <ExpenseForm
+            charge={charge}
+            handleSubmit={handleSubmit}
+            handleCharge={handleCharge}
+            amount={amount}
+            handleAmount={handleAmount}
+          />
         </div>
-      </main>
-    );
-  }
-}
+
+        <div
+          style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}
+        >
+          {/* Expense List */}
+          <ExpenseList initialExpenses={expenses} handleDelete={handleDelete} />
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "start",
+            marginTop: "1rem",
+          }}
+        >
+          <p style={{ fontSize: "2rem" }}>총합계:</p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
